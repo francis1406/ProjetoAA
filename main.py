fa17e53e4408ec4f4d78af3beac5fb7efd0b0a97
Francis Junior <44824972+francis1406@users.noreply.github.com>
2023-02-01 17:23:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def hello_world():  # put application's code here
+    return page1()
+
+@app.route('/letter1')
+def page1():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/letter2')
+def page2():  # put application's code here
+    return render_template('letter2.html')
+
+@app.route('/letter3')
+def page3():  # put application's code here
+    return render_template('letter3.html')
+
+@app.route('/letter4')
+def page4():  # put application's code here
+    return render_template('letter4.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
